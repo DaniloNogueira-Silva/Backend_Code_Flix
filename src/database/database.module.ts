ec0290d3b66ee92c5e1a178c576d25a843aa7e39
Danilo Nogueira <danilo.nogueira1802@gmail.com>
2024-05-21 13:49:18 -0300

    feat: criando módulo para banco de dados

---
+++ src/database/database.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from 'src/core/category/infra/db/sequelize/category.model';
+
+const models = [CategoryModel];
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite' as any,
+      host: ':memory:',
+      logging: false,
+      models,
+    }),
+  ],
+})
+export class DatabaseModule {}

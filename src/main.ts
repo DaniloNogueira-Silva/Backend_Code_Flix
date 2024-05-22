4e0917bb00102860702a3314c52a67043e25ee5c
Danilo Nogueira <danilo.nogueira1802@gmail.com>
2024-05-22 13:19:24 -0300

    feat: tratamento de erros not found

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { WrapperDataInterceptor } from './nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor';
+import { NotFoundErrorFilter } from './nest-modules/shared-module/filters/not-found-error.filter';
+import { EntityValidationErrorFilter } from './nest-modules/shared-module/filters/entity-validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +19,9 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.useGlobalInterceptors(new WrapperDataInterceptor());
+
+  //tratamento de erro global notfound
+  app.useGlobalFilters(new NotFoundErrorFilter(), new EntityValidationErrorFilter());
   await app.listen(3000);
 }
 bootstrap();

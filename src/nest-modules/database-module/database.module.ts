1b3932381de92e985780e118b9c8e3de6ee54910
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2024-06-26 18:54:58 -0300

    feat: create nest api for genres

--- src/nest-modules/database-module/database.module.ts
+++ src/nest-modules/database-module/database.module.ts
@@ -1,10 +1,21 @@
-import { Global, Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { Global, Module, Scope } from '@nestjs/common';
+import { SequelizeModule, getConnectionToken } from '@nestjs/sequelize';
 import { ConfigService } from '@nestjs/config';
 import { CONFIG_SCHEMA_TYPE } from '../config-module/config.module';
 import { CategoryModel } from '../../core/category/infra/db/sequelize/category.model';
+import { UnitOfWorkSequelize } from '../../core/shared/infra/db/sequelize/unit-of-work-sequelize';
+import { Sequelize } from 'sequelize';
+import {
+  GenreCategoryModel,
+  GenreModel,
+} from '../../core/genre/infra/db/sequelize/genre-model';
+import { CastMemberModel } from '../../core/cast-member/infra/db/sequelize/cast-member-sequelize';
+
 const models = [
   CategoryModel,
+  GenreModel,
+  GenreCategoryModel,
+  CastMemberModel,
 ];
 
 @Global()
@@ -42,5 +53,21 @@ const models = [
       inject: [ConfigService],
     }),
   ],
+  providers: [
+    {
+      provide: UnitOfWorkSequelize,
+      useFactory: (sequelize: Sequelize) => {
+        return new UnitOfWorkSequelize(sequelize);
+      },
+      inject: [getConnectionToken()],
+      scope: Scope.REQUEST,
+    },
+    {
+      provide: 'UnitOfWork',
+      useExisting: UnitOfWorkSequelize,
+      scope: Scope.REQUEST,
+    },
+  ],
+  exports: ['UnitOfWork'],
 })
 export class DatabaseModule {}
